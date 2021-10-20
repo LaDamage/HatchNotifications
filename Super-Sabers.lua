09575e33bb6c7c1f0e444537c97ef4a831cef36e
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2021-10-20 11:50:05 +0200

    Update Super-Sabers.lua

--- Super-Sabers.lua
+++ Super-Sabers.lua
@@ -2,9 +2,16 @@
 repeat wait() until game:IsLoaded()
 
 --// Core Variables
-local Rarity 
 local HttpService = game:GetService("HttpService")
 local HttpRequest = syn and syn.request or http_request
+
+--// Get Tier Variables
+local Tiers = {
+    "Golden",
+    "Diamond",
+    "Emerald",
+    "Celestial"
+}
 
 --// Game Variables
 local player = game:GetService("Players").LocalPlayer
@@ -24,13 +31,37 @@ end
 
 --// Pet Finder
 inventory.ChildAdded:Connect(function(instance)
+
     if instance.PetTemplate.Secret.Visible == true then
-        Rarity = instance.PetTemplate.Secret.Text
+
+        local Rarity = instance.PetTemplate.Secret.Text
+        local PetName = instance.PetTemplate.PetName.Text
+
+        local find_golden = string.find(PetName, "Golden")
+        local find_diamond = string.find(PetName, "Diamond")
+        local find_emerald = string.find(PetName, "Emerald")
+        local find_celestial = string.find(PetName, "Celestial")
+
+        if Rarity == "SECRET" then
+            embed_color = 0x55ffff
+        elseif Rarity == "MYTHIC" then
+            embed_color = 0xa020f0
+        end
+
+        for i, v in pairs(Tiers) do
+            if find_golden or find_diamond or find_emerald or find_celestial then
+                Send_Message = player.Name.." just crafted a "..Rarity.." "..PetName
+                embed_color = 0xffff33
+            else
+                Send_Message = player.Name.." just hatched a "..Rarity.." "..PetName
+            end
+        end
+
         local Data = {
             ["content"] = "",
             ["embeds"] = {{
-                ["title"] = player.Name.." just hatched a "..Rarity.." "..instance.PetTemplate.PetName.Text,
-                ["color"] =  tonumber(0x55ffff),
+                ["title"] = Send_Message,
+                ["color"] =  tonumber(embed_color),
                 ["fields"] = {{
                     ["name"] = "__Total Eggs Opened:__",
                     ["value"] = comma_value(game:GetService("Players").LocalPlayer.leaderstats["Eggs Opened"].Value),
