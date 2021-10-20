4a2f10138d90abee3905803496c0db27dcea68fa
CollateralDamage <75436158+LaDamage@users.noreply.github.com>
2021-10-20 10:04:18 +0200

    Update Super-Sabers.lua

--- Super-Sabers.lua
+++ Super-Sabers.lua
@@ -2,21 +2,15 @@
 repeat wait() until game:IsLoaded()
 
 --// Core Variables
+local Rarity 
 local HttpService = game:GetService("HttpService")
 local HttpRequest = syn and syn.request or http_request
 
 --// Game Variables
 local player = game:GetService("Players").LocalPlayer
-local chat = player.PlayerGui.Chat.Frame.ChatChannelParentFrame["Frame_MessageLogDisplay"].Scroller
+local inventory = game:GetService("Players").LocalPlayer.PlayerGui.Main.PetFrame.PetFrame.Pets.ScrollingFrame
 
 --// Get Functions
-function toHex(x) 
-    local hex =  string.format("%x", x)
-    return hex:len() == 1 and "0"..hex or hex
-end
-function RGB2HEX(r,g,b) 
-    return "0x" .. toHex(r) .. toHex(g) .. toHex(b)
-end
 function comma_value(amount)
     local formatted = amount
     while true do  
@@ -28,15 +22,15 @@ function comma_value(amount)
     return formatted
 end
 
---// Message Searching
-chat.ChildAdded:Connect(function(chat_message)
-    if chat_message.TextLabel.TextColor3 == Color3.fromRGB(85, 255, 255) then
-        local TextColor3 = chat_message.TextLabel.TextColor3
+--// Pet Finder
+inventory.ChildAdded:Connect(function(instance)
+    if instance.PetTemplate.Secret.Visible == true then
+        Rarity = instance.PetTemplate.Secret.Text
         local Data = {
             ["content"] = "",
             ["embeds"] = {{
-                ["title"] = player.Name.." just hatched a Secret or a Mythic!",
-                ["color"] =  tonumber(RGB2HEX(unpack({TextColor3.R*255,TextColor3.G*255,TextColor3.B*255}))),
+                ["title"] = player.Name.." just hatched a "..Rarity.." "..instance.PetTemplate.PetName.Text,
+                ["color"] =  tonumber(0x55ffff),
                 ["fields"] = {{
                     ["name"] = "__Total Eggs Opened:__",
                     ["value"] = comma_value(game:GetService("Players").LocalPlayer.leaderstats["Eggs Opened"].Value),
@@ -50,7 +44,7 @@ chat.ChildAdded:Connect(function(chat_message)
                 }
             }}
         }
-        
+
         HttpRequest({Url= _G.Webhook, Body = HttpService:JSONEncode(Data), Method = "POST", Headers = {["content-type"] = "application/json"}})
     end
 end)
